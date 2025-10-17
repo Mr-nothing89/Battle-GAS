ef1adf2f6ad3d36f45bea38b816e02fa071ed4de
Lucio Martinez <lu30ya31@hotmail.com>
2025-10-17 02:16:30 -0700

    Enemy Class

---
+++ Battle/Characters/CC_EnemyCharacter.cpp
@@ -0,0 +1,40 @@
+﻿// Lucio  (:  :(
+
+
+#include "CC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+// Sets default values
+ACC_EnemyCharacter::ACC_EnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ACC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ACC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(GetAbilitySystemComponent()))
+	{
+		return;
+	}
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this,this);
+
+	if (HasAuthority())
+	{
+		GiveStartupAbilities();
+	}
+}
+
