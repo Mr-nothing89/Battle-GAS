bdc165103546dda0acd47b88929bbee1aaea3456
Lucio Martinez <lu30ya31@hotmail.com>
2025-10-19 02:39:33 -0700

    Calculating Hit Direction

---
+++ Battle/Utils/CC_BlueprintLibrary.h
@@ -0,0 +1,30 @@
+﻿// Lucio  (:  :(
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CC_BlueprintLibrary.generated.h"
+
+UENUM(BlueprintType)
+enum class EHitDirection : uint8
+{
+	Left,
+	Right,
+	Forward,
+	Back
+};
+
+UCLASS()
+class BATTLE_API UCC_BlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure)
+	static EHitDirection GetHitDirection(const FVector& TargetForward,const FVector& ToInstigator);
+
+	UFUNCTION(BlueprintPure)
+	static FName GetHitDirectionName(const EHitDirection& HitDirection);
+};
